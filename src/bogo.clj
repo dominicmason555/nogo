bbba66e35933af09068d898ec6f27a0670d0189b
Dominic Mason <dominicmason555@gmail.com>
2023-12-28 22:17:19 +0000

    Got ATOM generation working

--- src/bogo.clj
+++ src/bogo.clj
@@ -1,11 +1,14 @@
 (ns bogo
   (:require [clojure.string :as string]
             [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]
             [me.raynes.fs :as fs]
             [hickory.core :as hick]
             [hickory.render :refer [hickory-to-html]]
             [cljstache.core :as stache]))
+
+(def atom-template (io/resource "atom_templ.xml"))
 
 (defn output-rendered "Loops over the pages to rendering and outputting them"
   [data]
@@ -26,7 +29,7 @@
                                                 :header header
                                                 :footer footer
                                                 :main main})]
-          (println (str "Rendering " outfile))
+          (println (str "Outputting page: " outfile))
           (spit outfile rendered))))))
 
 (defn page-to-twtxt "Creates a TWTXT entry from a page"
@@ -49,8 +52,31 @@
         outfile (fs/file outfolder "twtxt.txt")
         twtxt (string/join "\n" (map page-to-twtxt (get-meta data)))]
     (fs/mkdirs outfolder)
-    (println (str "Outputting Feed: " outfile))
+    (println (str "Outputting feed: " outfile))
     (spit outfile twtxt)))
+
+(defn generate-atom "Generates and outputs the ATOM feed"
+  [data time]
+  (let [outfolder (fs/file (data :rootpath) "output" ((data :data) :feedsdir))
+        outfile (fs/file outfolder "atom.xml")
+        template (slurp atom-template)
+        posts (get-meta data)
+        authorname (get-in data [:data :authorname])
+        feedid (get-in data [:data :feedid])
+        feedtitle (get-in data [:data :title])
+        feedlink (get-in data [:data :url])
+        selflink (string/join "/" [(get-in data [:data :url])
+                                   (get-in data [:data :feedsdir]) "atom.xml"])
+        rendered (stache/render template {:entries posts
+                                          :feedtitle feedtitle
+                                          :feedlink feedlink
+                                          :selflink selflink
+                                          :authorname authorname
+                                          :feedid feedid
+                                          :updated time})]
+    (fs/mkdirs outfolder)
+    (println (str "Outputting feed: " outfile))
+    (spit outfile rendered)))
 
 (defn get-data "Parses the Nogo EDN file"
   [path]
@@ -65,6 +91,8 @@
   (let [data (get-data path)]
     (println "Generating feeds")
     (generate-twtxt data)
+    (generate-atom data (.. (java.time.ZonedDateTime/now)
+                            (format java.time.format.DateTimeFormatter/ISO_INSTANT)))
     (println "Generating pages")
     (output-rendered data))
   (println "Generation complete"))
